460b2188cc50c076cfd08885fa1312698f101950
Martin Gelder <darkspotinthecorner@gmail.com>
2012-09-17 23:52:33 +0200

    blubb

--- courses/0 - Playground/index.php
+++ courses/0 - Playground/index.php
@@ -17,10 +17,9 @@ $impress = new ImpressJS();
 		<meta name="description" content="" />
 		<meta name="author" content="Martin Gelder" />
 
-		<!-- Bootstrap -->
-		<link href="../assets/css/bootstrap.css" rel="stylesheet" />
+		<link href="../assets/bootstrap/bootstrap.css" rel="stylesheet" />
 		<link href="../assets/css/generic.css" rel="stylesheet" />
-		<!-- <link href="../../impress-demo/style.css" rel="stylesheet" /> -->
+		<link href="../assets/prettify/prettify.css" rel="stylesheet" />
 		<link href="style.css" rel="stylesheet" />
 
 		<link rel="shortcut icon" href="../assets/icons/favicon.png" />
@@ -35,77 +34,144 @@ $impress = new ImpressJS();
 			</div>
 		</div>
 		<div id="impress">
-			<div id="aaa" class="step slide" <?= $impress ?>>
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-			</div>
-			<div id="bbb" class="step slide" <?= $impress->moveRight(1000)->rotateY(5) ?>>
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-			</div>
-			<div id="ccc" class="step slide" <?= $impress->moveRight(1000)->rotateY(5) ?>>
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-			</div>
-			<div id="ddd" class="step slide" <?= $impress->moveRight(1000)->rotateY(5) ?>>
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-			</div>
-			<div id="eee" class="step slide" <?= $impress->moveRight(1000)->rotateY(5) ?>>
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-			</div>
-<?php
-			/*
-			<div id="bored" class="step slide" <?= $impress->shiftX(-1000)->shiftY(-1500) ?>>
-				<q>Aren't you just <b>bored</b> with all those slides-based presentations?</q>
+			<div class="step slide" <?= $impress ?>>
+				<h1>CSS - Grundlagen</h1>
+				<h2>Martin Gelder 2012</h2>
 			</div>
 			<div class="step slide" <?= $impress->shiftX(1000) ?>>
-				<q>Don't you think that presentations given <strong>in modern browsers</strong> shouldn't <strong>copy the limits</strong> of 'classic' slide decks?</q>
+				<h1>Was ist CSS?</h1>
+				<ul>
+					<li>CSS steht für <b>C</b>ascading <b>S</b>tyle <b>S</b>heets</li>
+					<li>Es handelt sich um eine deklarative Sprache</li>
+					<li>Dient als Stilvorlage für strukturierte Dokumente (meist HTML)</li>
+				</ul>
 			</div>
 			<div class="step slide" <?= $impress->shiftX(1000) ?>>
-				<q>Would you like to <strong>impress your audience</strong> with <strong>stunning visualization</strong> of your talk?</q>
+				<h1>Einbinden von CSS</h1>
+				<p>Inline <span class="sub">(schlechte Idee&trade;)</span></p>
+				<p><pre class="prettyprint lang-html"><?= htmlspecialchars('<div style="Deklaration(en)"> ... </div>') ?></pre></p>
+				<p>Eingebettet <span class="sub">(bevorzugt im <code class="prettyprint lang-html"><?= htmlspecialchars('<head>') ?></code>)</span></p>
+				<p><pre class="prettyprint lang-html"><?= htmlspecialchars('<style type="text/css"> {CSS Code} </style>') ?></pre></p>
+				<p>Extern <span class="sub">(bevorzugt im <code class="prettyprint lang-html"><?= htmlspecialchars('<head>') ?></code>)</span></p>
+				<p><pre class="prettyprint lang-html"><?= htmlspecialchars('<link href="datei.css" type="text/css" />') ?></pre></p>
 			</div>
-			<div id="title" class="step" <?= $impress->shiftX(-1000)->shiftY(1500)->setScale(4) ?>>
-				<span class="try">then you should try</span>
-				<h1>impress.js<sup>*</sup></h1>
-				<span class="footnote"><sup>*</sup> no rhyme intended</span>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1>CSS Code</h1>
+				<p>Ein CSS Code Block kann beliebig viele Regeln aufnehmen</p>
+				<p><pre class="prettyprint lang-css">
+/* Schreibweise einer Regel:
+Selektor(en) {
+	Deklaration(en)
+}
+*/
+
+body {
+	font-family: Tahoma, Arial, sans-serif;
+	font-size: 12px;
+}
+
+header, footer { font-size: 14px; }</pre></p>
 			</div>
-			<div id="its" class="step" <?= $impress->shiftX(850)->shiftY(3000)->rotateZ(90)->setScale(5) ?>>
-				<p>It's a <strong>presentation tool</strong> <br/>
-				inspired by the idea behind <a href="http://prezi.com">prezi.com</a> <br/>
-				and based on the <strong>power of CSS3 transforms and transitions</strong> in modern browsers.</p>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1>Selektoren</h1>
+				<p>Selektoren legen fest auf welche HTML Elemente die Regel zutrifft</p>
+				<p>Es gibt 3 direkte Selektorarten</p>
+				<p><pre class="prettyprint lang-css">
+a     { /* Alle &lt;a&gt; Elemente */ }
+
+.info { /* Alle Elemente mit class="info" */ }
+
+#main { /* Das Element mit id="main" */ }
+</pre></p>
 			</div>
-			<div id="big" class="step" <?= $impress->shiftX(2650)->shiftY(-900)->rotateZ(90)->setScale(6) ?>>
-				<p>visualize your <b>big</b> <span class="thoughts">thoughts</span></p>
+
+
+
+
+
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1>Deklarationen</h1>
+				<p>Belegen eine Eigenschaft mit einem Wert(en)</p>
+				<p><pre class="prettyprint lang-css">
+/* Schreibweise einer Deklaration:
+Eigenschaft : Wert(e) ;
+*/
+
+color : #ffbb00 ;</pre></p>
+				<p>Beliebig viele Deklarationen pro Block</p>
+				<p>Trennung durch ein Semikolon</p>
 			</div>
-			<div id="tiny" class="step" <?= $impress->shiftX(-175)->shiftY(225)->shiftZ(-3000)->rotateZ(120)->setScale(1) ?>>
-				<p>and <b>tiny</b> ideas</p>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1>Verkürzte Deklarationen</h1>
+				<p>Die ausführliche Schreibweise ...</p>
+				<p><pre class="prettyprint lang-css">
+padding-top: 3px;
+padding-right: 1em;
+padding-bottom: 15px;
+padding-left: 1.5em;</pre></p>
+				<p>... kann auch verkürzt notiert werden</p>
+				<p><pre class="prettyprint lang-css">padding: 3px 1em 15px 1.5em;</pre></p>
 			</div>
-			<div id="ing" class="step" <?= $impress->shiftZ(3000)->shiftY(-3175)->shiftX(175)->rotateZ(-30)->setScale(6) ?>>
-				<p>by <b class="positioning">positioning</b>, <b class="rotating">rotating</b> and <b class="scaling">scaling</b> them on an infinite canvas</p>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1></h1>
+				<p></p>
 			</div>
-			<div id="imagination" class="step" <?= $impress->shiftX(3200)->shiftY(550)->setRotZ(0) ?>>
-				<p>the only <b>limit</b> is your <b class="imagination">imagination</b></p>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1></h1>
+				<p></p>
 			</div>
-			<div id="source" class="step" <?= $impress->shiftX(-500)->shiftY(2300)->rotateZ(20)->setScale(4) ?>>
-				<p>want to know more?</p>
-				<q><a href="http://github.com/bartaz/impress.js">use the source</a>, Luke!</q>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1></h1>
+				<p></p>
 			</div>
-			<div id="source2" class="step" <?= $impress->moveRight(600) ?>>
-				<p>want to know even more?</p>
-				<q><a href="http://github.com/bartaz/impress.js">use the source</a>, Luke!</q>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1></h1>
+				<p></p>
 			</div>
-			<div id="one-more-thing" class="step" <?= $impress->shiftX(-200)->shiftY(2000)->rotateZ(-20)->setScale(2) ?>>
-				<p>one more thing...</p>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1></h1>
+				<p></p>
 			</div>
-			<div id="its-in-3d" class="step" <?= $impress->shiftX(200)->shiftY(300)->shiftZ(-100)->rotateX(-40)->rotateY(10) ?>>
-				<p><span class="have">have</span> <span class="you">you</span> <span class="noticed">noticed</span> <span class="its">it's</span> <span class="in">in</span> <b>3D<sup>*</sup></b>?</p>
-				<span class="footnote">* beat that, prezi ;)</span>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1></h1>
+				<p></p>
 			</div>
-			<div id="overview" class="step" <?= $impress->setPosx(3000)->setPosY(1500)->setPosZ(0)->setScale(10)->resetRotation() ?>>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1></h1>
+				<p></p>
 			</div>
-			*/
-?>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1></h1>
+				<p></p>
+			</div>
+			<div class="step slide" <?= $impress->shiftX(1000) ?>>
+				<h1></h1>
+				<p></p>
+			</div>
+
+
+
+
+
+
+
+
 		</div>
+
 		<script src="../assets/js/jquery-1.8.1.js"></script>
-		<script src="../assets/js/bootstrap.js"></script>
+
+		<script src="../assets/bootstrap/bootstrap.js"></script>
+
+		<script src="../assets/prettify/prettify.js"></script>
+		<script src="../assets/prettify/lang-css.js"></script>
+
 		<script src="../assets/js/impress.js"></script>
-		<script>impress().init();</script>
+
+		<script>
+			$(function() {
+				impress().init();
+				prettyPrint();
+			});
+		</script>
 	</body>
 </html>
--- courses/inc/ImpressJS.php
+++ courses/inc/ImpressJS.php
@@ -22,35 +22,35 @@ class ImpressJS {
 		return $this;
 	}
 
-	public function resetPosition()      { $this->position = array(0, 0, 0);                         return $this; }
-	public function resetRotation()      { $this->rotation = array(0, 0, 0);                         return $this; }
+	public function resetPosition()      { $this->position = array(0, 0, 0);                                 return $this; }
+	public function resetRotation()      { $this->rotation = array(0, 0, 0);                                 return $this; }
 
-	public function setPosX($value = 0)  { $this->position[0] = intval($value);                      return $this; }
-	public function setPosY($value = 0)  { $this->position[1] = intval($value);                      return $this; }
-	public function setPosZ($value = 0)  { $this->position[2] = intval($value);                      return $this; }
+	public function setPosX($value = 0)  { $this->position[0] = intval($value);                              return $this; }
+	public function setPosY($value = 0)  { $this->position[1] = intval($value);                              return $this; }
+	public function setPosZ($value = 0)  { $this->position[2] = intval($value);                              return $this; }
 
-	public function setRotX($value = 0)  { $this->rotation[0] = intval($value);                      return $this; }
-	public function setRotY($value = 0)  { $this->rotation[1] = intval($value);                      return $this; }
-	public function setRotZ($value = 0)  { $this->rotation[2] = intval($value);                      return $this; }
+	public function setRotX($value = 0)  { $this->rotation[0] = intval($value);                              return $this; }
+	public function setRotY($value = 0)  { $this->rotation[1] = intval($value);                              return $this; }
+	public function setRotZ($value = 0)  { $this->rotation[2] = intval($value);                              return $this; }
 
-	public function setScale($value = 1) { $this->scale = intval($value);                            return $this; }
+	public function setScale($value = 1) { $this->scale = intval($value);                                    return $this; }
 
-	public function shiftX($value = 0)   { $this->position[0] = $this->position[0] + intval($value); return $this; }
-	public function shiftY($value = 0)   { $this->position[1] = $this->position[1] + intval($value); return $this; }
-	public function shiftZ($value = 0)   { $this->position[2] = $this->position[2] + intval($value); return $this; }
+	public function shiftX($value = 0)   { $this->applyVectorToPosition(array(intval($value), 0, 0), false); return $this; }
+	public function shiftY($value = 0)   { $this->applyVectorToPosition(array(0, intval($value), 0), false); return $this; }
+	public function shiftZ($value = 0)   { $this->applyVectorToPosition(array(0, 0, intval($value)), false); return $this; }
 
-	public function rotateX($value = 0)  { $this->rotation[0] = $this->rotation[0] + intval($value); return $this; }
-	public function rotateY($value = 0)  { $this->rotation[1] = $this->rotation[1] + intval($value); return $this; }
-	public function rotateZ($value = 0)  { $this->rotation[2] = $this->rotation[2] + intval($value); return $this; }
+	public function rotateX($value = 0)  { $this->rotation[0] = $this->rotation[0] + intval($value);         return $this; }
+	public function rotateY($value = 0)  { $this->rotation[1] = $this->rotation[1] + intval($value);         return $this; }
+	public function rotateZ($value = 0)  { $this->rotation[2] = $this->rotation[2] + intval($value);         return $this; }
 
-	public function scale($value = 1)    { $this->scale = $this->scale * intval($value);             return $this; }
+	public function scale($value = 1)    { $this->scale = $this->scale * intval($value);                     return $this; }
 
-	public function moveLeft($value)     { $this->applyVectorToPosition(array(-intval($value), 0, 0)); return $this; }
-	public function moveRight($value)    { $this->applyVectorToPosition(array( intval($value), 0, 0)); return $this; }
-	public function moveUp($value)       { $this->applyVectorToPosition(array(0, -intval($value), 0)); return $this; }
-	public function moveDown($value)     { $this->applyVectorToPosition(array(0,  intval($value), 0)); return $this; }
-	public function moveForward($value)  { $this->applyVectorToPosition(array(0, 0,  intval($value))); return $this; }
-	public function moveBackward($value) { $this->applyVectorToPosition(array(0, 0, -intval($value))); return $this; }
+	public function moveLeft($value)     { $this->applyVectorToPosition(array(-intval($value), 0, 0));       return $this; }
+	public function moveRight($value)    { $this->applyVectorToPosition(array( intval($value), 0, 0));       return $this; }
+	public function moveUp($value)       { $this->applyVectorToPosition(array(0, -intval($value), 0));       return $this; }
+	public function moveDown($value)     { $this->applyVectorToPosition(array(0,  intval($value), 0));       return $this; }
+	public function moveForward($value)  { $this->applyVectorToPosition(array(0, 0,  intval($value)));       return $this; }
+	public function moveBackward($value) { $this->applyVectorToPosition(array(0, 0, -intval($value)));       return $this; }
 
 	public function __toString()
 	{
@@ -91,6 +91,48 @@ class ImpressJS {
 		);
 	}
 
+	protected function getRotationMatrixFromAnglesNew($x, $y, $z)
+	{
+		$matrix = array(array(1, 0, 0), array(0, 1, 0), array(0, 0, 1));
+		$store  = array(array(0, 0, 0), array(0, 0, 0), array(0, 0, 0));
+
+		$rot_x = ($x * M_PI) / 180;
+		$rot_y = ($y * M_PI) / 180;
+		$rot_z = ($z * M_PI) / 180;
+
+		$rotator = array(
+			array(array(1, 0, 0), array(0, cos($rot_x), sin($rot_x)), array(0, -sin($rot_x), cos($rot_x))),
+			array(array(cos($rot_y), 0, -sin($rot_y)), array(0, 1, 0), array(sin($rot_y), 0, cos($rot_y))),
+			array(array(cos($rot_z), sin($rot_z), 0), array(-sin($rot_z), cos($rot_z), 0), array(0, 0, 1)),
+		);
+
+		foreach($rotator as $rotid => $rotation)
+		{
+			for($i = 0; $i < 3; $i++)
+			{
+				for($j = 0; $j < 3; $j++)
+				{
+					$store[$i][$j] = 0;
+
+					for($n = 0; $n < 3; $n++)
+					{
+						$store[$i][$j] += $matrix[$i][$n] * $rotation[$n][$j];
+					}
+				}
+			}
+
+			for($i = 0; $i < 3; $i++)
+			{
+				for($j = 0; $j < 3; $j++)
+				{
+					$matrix[$i][$j] = $store[$i][$j];
+				}
+			}
+		}
+
+		return $matrix;
+	}
+
 	protected function applyRotationMatrixToVector($vector, $matrix)
 	{
 		return array(
@@ -104,7 +146,7 @@ class ImpressJS {
 	{
 		if($relative === true)
 		{
-			$vector = $this->applyRotationMatrixToVector($vector, $this->getRotationMatrixFromAngles($this->rotation[0], $this->rotation[1], $this->rotation[2]));
+			$vector = $this->applyRotationMatrixToVector($vector, $this->getRotationMatrixFromAnglesNew($this->rotation[0], $this->rotation[1], $this->rotation[2]));
 		}
 
 		$position_new = array(
@@ -116,5 +158,19 @@ class ImpressJS {
 		$this->position = $position_new;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
